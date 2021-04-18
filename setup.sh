a0098b9cd3ed3f9338c4089b8e390188955c7bd3
Sam Lavigne <splavigne@gmail.com>
2021-04-18 16:34:55 -0400

    add audio to setup

--- setup.sh
+++ setup.sh
@@ -6,6 +6,22 @@ adduser --disabled-password --gecos "Synthetic" syn
 # set the start up script
 mkdir -p /home/syn/.config/openbox/
 echo "ffmpeg -f x11grab -r 24 -s 1280x720 -i :1.0+0,0 -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video1 &" > /home/syn/.config/openbox/autostart
+
+# set virtual audio to start on boot
+mkdir -p /home/syn/.config/pulse/
+cat > /home/syn/.config/pulse/default.pa <<EOL
+.include /etc/pulse/default.pa
+
+.ifexists module-null-sink.so
+load-module module-null-sink sink_name=Source
+.endif
+
+.ifexists module-virtual-source.so
+load-module module-virtual-source source_name=VirtualMic master=Source.monitor
+.endif
+EOL
+
+# fix permissions
 chown syn:syn /home/syn/.config/
 chown -R syn:syn /home/syn/.config/
 
@@ -29,7 +45,7 @@ systemctl restart ssh
 
 # install the stuff
 apt update
-apt -y install openbox obconf ffmpeg tigervnc-standalone-server tigervnc-common xdg-utils wmctrl xterm git libnss3 xdotool x11-xserver-utils feh
+apt -y install openbox obconf ffmpeg tigervnc-standalone-server tigervnc-common xdg-utils wmctrl xterm git libnss3 xdotool x11-xserver-utils feh pulseaudio pulseaudio-utils alsa-utils
 apt -y install linux-headers-$(uname -r)
 apt -y install linux-modules-extra-$(uname -r)
 apt -y install dkms
@@ -53,12 +69,22 @@ options v4l2loopback exclusive_caps=1
 options v4l2loopback video_nr=1
 EOL
 
+# virtual audio
+# sudo modprobe snd-dummy
+# sudo modprobe snd-aloop
+pactl load-module module-null-sink sink_name=Source
+pactl load-module module-virtual-source source_name=VirtualMic master=Source.monitor
+
+
 # install node
 curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install -y nodejs
 
 # add user to video group
 usermod -a -G video syn
+
+# add user to audio group
+usermod -a -G audio syn
 
 # create service for vnc
 cat > /etc/systemd/system/vncserver.service <<EOL
