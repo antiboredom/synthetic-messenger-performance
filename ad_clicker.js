c4a474e7482b89cd4f23624f38518860248dcd25
Sam Lavigne <splavigne@gmail.com>
2021-05-29 13:14:53 -0400

    blacklist

--- ad_clicker.js
+++ ad_clicker.js
@@ -33,6 +33,15 @@ let selectors = fs
   .split("\n")
   .filter((s) => s.trim() != "");
 
+const blacklist = new RegExp(
+  fs
+    .readFileSync("blacklist.txt", { encoding: "utf8" })
+    .split("\n")
+    .filter(s => s.trim() != "")
+    .join("|")
+    .replace(/\./g, "[.]")
+);
+
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -57,6 +66,7 @@ async function getBBox(el) {
 async function getRecentArticles() {
   try {
     let results = await getJSON(`${api}/articles?host=${hostname}&key=${KEY}`);
+    results = results.filter(r => !blacklist.test(r));
     return results;
   } catch (e) {
     console.log(e);
